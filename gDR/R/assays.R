7b1234d1b0741294dfd4181d76188e71739ec1df
potockan <natalia.potocka@contractors.roche.com>
2019-11-12 10:58:58 +0100

    fix styling in assays

--- gDR/R/assays.R
+++ gDR/R/assays.R
@@ -14,7 +14,7 @@
   function(drug_data) {
     as.data.frame(drug_data) %>%
       dplyr::filter(grepl(.untreateDrugNameRegex, DrugName)) %>%
-      dplyr::pull('name_')
+      dplyr::pull("name_")
   }
 
 #' .get_treated_conditions
@@ -29,7 +29,7 @@
   function(drug_data) {
     as.data.frame(drug_data) %>%
       dplyr::filter(!grepl(.untreateDrugNameRegex, DrugName)) %>%
-      dplyr::pull('name_')
+      dplyr::pull("name_")
   }
 
 
@@ -52,83 +52,83 @@ getMetaData <- function(data,
     setdiff(
       colnames(data),
       c(
-        gDR::get_header('raw_data'),
-        gDR::get_header('normalized_results'),
-        gDR::get_header('averaged_results'),
-        gDR::get_header('metrics_results'),
-        gDR::get_identifier('WellPosition'),
-        'Barcode',
-        'Template',
+        gDR::get_header("raw_data"),
+        gDR::get_header("normalized_results"),
+        gDR::get_header("averaged_results"),
+        gDR::get_header("metrics_results"),
+        gDR::get_identifier("WellPosition"),
+        "Barcode",
+        "Template",
         # not sure how to handle these ones ....    < --------
-        'Concentration'
+        "Concentration"
       )
     ) # remove as it will be the third dimension
   # find all unique conditions
   conditions <- unique(data[, metavars])
   # get the metadata not directly related to cells
   nocell_metavars <- setdiff(metavars,
-                             c(get_identifier('cellline'), gDR::get_header('add_clid')))
+                             c(get_identifier("cellline"), gDR::get_header("add_clid")))
   
   constant_metavars <-
     setdiff(
       nocell_metavars[sapply(nocell_metavars,
                              function(x)
-                               nrow(unique(conditions[, x, drop = F]))) == 1],
+                               nrow(unique(conditions[, x, drop = FALSE]))) == 1],
       # protect cell line and drug name
       c(
-        gDR::get_header('add_clid'),
-        gDR::get_identifier('drug'),
-        gDR::get_identifier('drugname')
+        gDR::get_header("add_clid"),
+        gDR::get_identifier("drug"),
+        gDR::get_identifier("drugname")
       )
     )
   unique_metavars <- c(intersect(
     c(
-      gDR::get_identifier('cellline'),
-      gDR::get_header('add_clid'),
-      gDR::get_identifier('drug'),
-      gDR::get_identifier('drugname')
+      gDR::get_identifier("cellline"),
+      gDR::get_header("add_clid"),
+      gDR::get_identifier("drug"),
+      gDR::get_identifier("drugname")
     ),
     metavars
   ),
   nocell_metavars[sapply(nocell_metavars, function(x)
-    nrow(unique(conditions[, x, drop = F]))) > 1])
+    nrow(unique(conditions[, x, drop = FALSE]))) > 1])
   
   # find the cell lines and related data (for the columns in the SE)
   cl_entries <- cell_id
   for (j in setdiff(unique_metavars, cell_id)) {
     if (nrow(unique(conditions[, c(cell_id, j)])) ==
-        nrow(unique(conditions[, cell_id, drop = F]))) {
+        nrow(unique(conditions[, cell_id, drop = FALSE]))) {
       cl_entries = c(cl_entries, j)
     }
   }
   # --> not very robust --> need testing
   cl_entries <- setdiff(cl_entries,
                         c(
-                          gDR::get_identifier('drug'),
-                          paste0(gDR::get_identifier('drug'), '_', 2:10),
-                          gDR::get_identifier('drugname'),
-                          paste0(gDR::get_identifier('drugname'), '_', 2:10)
+                          gDR::get_identifier("drug"),
+                          paste0(gDR::get_identifier("drug"), "_", 2:10),
+                          gDR::get_identifier("drugname"),
+                          paste0(gDR::get_identifier("drugname"), "_", 2:10)
                         ))
   
   # temporary removing extra column to avoid bug
-  cl_entries <- setdiff(cl_entries, 'ReferenceDivisionTime')
+  cl_entries <- setdiff(cl_entries, "ReferenceDivisionTime")
   
   #colData
-  colData <- unique(conditions[, cl_entries, drop = F])
+  colData <- unique(conditions[, cl_entries, drop = FALSE])
   colData$col_id <- 1:nrow(colData)
   colData$name_ <-
     apply(colData, 1, function(x)
-      paste(x, collapse = '_'))
+      paste(x, collapse = "_"))
   
   # get all other metadata for the rows
   cond_entries <- setdiff(unique_metavars, cl_entries)
   # temporary removing extra column to avoid bug
-  cond_entries <- setdiff(cond_entries, 'ReferenceDivisionTime')
-  rowData <- unique(conditions[, cond_entries, drop = F])
+  cond_entries <- setdiff(cond_entries, "ReferenceDivisionTime")
+  rowData <- unique(conditions[, cond_entries, drop = FALSE])
   rowData$row_id <- 1:nrow(rowData)
   rowData$name_ <-
     apply(rowData, 1, function(x)
-      paste(x, collapse = '_'))
+      paste(x, collapse = "_"))
   
   # get the remaining columns as data
   dataCols <- setdiff(colnames(data), metavars)
@@ -143,14 +143,14 @@ getMetaData <- function(data,
 #' df_to_assay
 #'
 #' Convert data.frame with dose-reponse data to the assay.
-#' 
+#'
 #' The 'assay' object is simply a matrix with rownames being the treatment ids,
-#' colnames being the ids of the cell lines and values being the DataFrames with 
+#' colnames being the ids of the cell lines and values being the DataFrames with
 #' dose-response data for given cell lines under given conditions.
 #'
 #' @param data tibble or data.frame with drug-response data
 #' @param treatment_id string id of the column with the treatment conditions data (in 'data' object)
-#' @param data_type string type of data to be returned: all, for untreated conditions only ('untreated') 
+#' @param data_type string type of data to be returned: all, for untreated conditions only ('untreated')
 #' or for treated conditions only ('treated')
 #'
 #' @return matrix
@@ -167,10 +167,10 @@ df_to_assay <-
     allMetadata <- getMetaData(data)
     
     seColData <- allMetadata$colData
-    cl_entries <- setdiff(colnames(seColData), c('col_id', 'name_'))
+    cl_entries <- setdiff(colnames(seColData), c("col_id", "name_"))
     seRowData <- allMetadata$rowData
     cond_entries <-
-      setdiff(colnames(seRowData), c('row_id', 'name_'))
+      setdiff(colnames(seRowData), c("row_id", "name_"))
     dataCols <- allMetadata$dataCols
     
     complete <-
@@ -181,8 +181,8 @@ df_to_assay <-
           stringsAsFactors = FALSE
         )
       )
-    complete <- merge(merge(complete, seRowData, by = 'row_id'),
-                      seColData, by = 'col_id')
+    complete <- merge(merge(complete, seRowData, by = "row_id"),
+                      seColData, by = "col_id")
     complete$factor_id <- 1:nrow(complete)
     data_assigned <-
       merge(data, complete, by = c(cond_entries, cl_entries))
@@ -217,10 +217,10 @@ df_to_assay <-
     if (data_type == "untreated") {
       untreatedConds <-
         .get_untreated_conditions(seRowData)
-      return(final.set[rownames(final.set) %in% untreatedConds,])
+      return(final.set[rownames(final.set) %in% untreatedConds, ])
     } else if (data_type == "treated") {
       treatedConds <- .get_treated_conditions(seRowData)
-      return(final.set[rownames(final.set) %in% treatedConds,])
+      return(final.set[rownames(final.set) %in% treatedConds, ])
     } else if (data_type == "all") {
       return(final.set)
     } else {
