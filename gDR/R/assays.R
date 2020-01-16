c11e3b1ae0b0870f0ba531cb0bcef17da90d1181
scigockd <dariusz.scigocki@contractors.roche.com>
2020-01-16 19:12:40 +0000

    fixed colnames in assay_to_df

--- gDR/R/assays.R
+++ gDR/R/assays.R
@@ -399,19 +399,20 @@ assay_to_df <- function(se, assay_name, merge_metrics = FALSE) {
     if (merge_metrics) {
       old_colnames <- c("x_mean", "x_AOC", "xc50", "x_max", "c50", "x_inf", "x_0", 
                         "h", "r2", "flat_fit", "maxlog10Concentration", "N_conc")
-      new_colnames <- gsub("x_", "", old_colnames)
-      IC_colnames <- paste("IC", new_colnames, sep = "_")
-      GR_colnames <- paste("GR", new_colnames, sep = "_")
-      
+      new_colnames_IC = c("mean_viability", "ic_AOC", "ic50", "e_max", "ec50",
+                          "e_inf", "e_0", "h_ic", "ic_r2", "flat_fit_ic")
+      new_colnames_GR = c("mean_GR", "GR_AOC", "GR50", "GR_max", "GEC50",
+                          "GR_inf", "GR_0", "h_GR", "GR_r2", "flat_fit_GR")
+
       Df_IC <- subset(asDf, dr_metric == "IC") %>% dplyr::select(-dr_metric)
       Df_GR <- subset(asDf, dr_metric == "GR", select = c("rId", "cId", old_colnames))
       
       data.table::setnames(Df_IC, 
                            old = old_colnames, 
-                           new = IC_colnames)
+                           new = new_colnames_IC)
       data.table::setnames(Df_GR, 
                            old = old_colnames, 
-                           new = GR_colnames)
+                           new = new_colnames_GR)
       asDf <- dplyr::full_join(Df_IC, Df_GR, by = c("rId", "cId"))
     }
   }
