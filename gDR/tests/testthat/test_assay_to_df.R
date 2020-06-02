5bef147b4b0527056d0fc3232e4ad91180ab72b4
bczech <bartosz.czech@contractors.roche.com>
2020-06-02 13:25:00 +0000

    Add assay_to_df.R test

---
+++ gDR/tests/testthat/test_assay_to_df.R
@@ -0,0 +1,11 @@
+testthat::context("Test assay_to_df function")
+
+testthat::test_that("Test assay_to_df works", {
+
+seL_file <- system.file(package = "gDR", "testdata/seL.rds")
+seL <- readRDS(seL_file)
+se <- seL[[3]]
+test1 = assay_to_df(se, "Avg_Controls")
+test2 = assay_to_df(se, 4)
+
+}
