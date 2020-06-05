a8d0f523e848e6f17475b2fee652c988853fcd36
bczech <bartosz.czech@contractors.roche.com>
2020-06-05 10:14:05 +0000

    Update test_assay_to_df and restore Dockerfile from master

--- gDR/tests/testthat/test_assay_to_df.R
+++ gDR/tests/testthat/test_assay_to_df.R
@@ -1,3 +1,4 @@
+library(testthat)
 testthat::context("Test assay_to_df function")
 
 testthat::test_that("Test assay_to_df works", {
@@ -5,7 +6,14 @@ testthat::test_that("Test assay_to_df works", {
 seL_file <- system.file(package = "gDR", "testdata/seL.rds")
 seL <- readRDS(seL_file)
 se <- seL[[3]]
-test1 = assay_to_df(se, "Avg_Controls")
-test2 = assay_to_df(se, 4)
+test1 <-  assay_to_df(se, "Avg_Controls")
+test2 <-  assay_to_df(se, 4)
+expect_equal(test1, test2)
+
+test1 <- test1 %>%
+  dplyr::select(-CellLineName, -E2)
+test2 <- test2 %>%
+  dplyr::select(-CellLineName, -E2)
+expect_equal(test1, test2)
 
 })
