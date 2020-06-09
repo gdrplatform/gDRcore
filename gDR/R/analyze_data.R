971f40dd4a82730cae152ae1d703708ecdec44ed
Marc Hafner <hafnerm6@gene.com>
2020-06-09 11:17:58 -0700

    Annotating combination treatments and testing on GDS data

--- gDR/R/analyze_data.R
+++ gDR/R/analyze_data.R
@@ -1008,7 +1008,8 @@ mapSE <- function(normSE, ctrlSE, row_endpoint_value_filter, Keys, T0 = FALSE){
       match_idx <- which(apply(as.matrix(match_mx), 2, all)) # test matching conditions
       if (length(match_idx) == 0) {
         # if not exact match, try to find best match (as many metadata fields as possible)
-        futile.logger::flog.warn("Missing treated contols for: %s", x)
+        futile.logger::flog.warn("Missing untreated controls %s for: %s",
+            ifelse(T0, '(T=0)', '(endpoint)'), x)
         idx <-
           apply(as.matrix(match_mx), 2, function(y)
             sum(y, na.rm = TRUE)) *
@@ -1024,3 +1025,73 @@ mapSE <- function(normSE, ctrlSE, row_endpoint_value_filter, Keys, T0 = FALSE){
       return(rownames(ctrlSE)[match_idx])
     })
 }
+
+
+
+add_codrug_group = function(SE) {
+
+  r_data = SummarizedExperiment::rowData(SE)
+  if (!('Gnumber_2' %in% colnames(r_data))) return(SE)
+
+  # find the pairs of drugs with relevant metadata
+  drug_ids = paste0(get_identifier()$drug, c('', '_2'))
+  other_metadata = setdiff(colnames(r_data), c('Concentration_2', drug_ids,
+                    paste0(get_identifier()$drugname, c('', paste0('_',1:10)))))
+  drug_pairs = unique(r_data[, c(drug_ids, other_metadata)])
+  drug_pairs = drug_pairs[ !(drug_pairs[,drug_ids[2]] %in% get_identifier('untreated_tag')),]
+
+  pair_list = vector('list', nrow(drug_pairs))
+  # loop through the pairs to assess the number of individual concentration pairs
+  for (idp in 1:nrow(drug_pairs)) {
+    row_idx = r_data[,drug_ids[1]] %in% unlist(drug_pairs[idp, drug_ids]) &
+            r_data[,drug_ids[2]] %in% c(unlist(drug_pairs[idp, drug_ids]),
+                get_identifier('untreated_tag')) &
+            apply(as.matrix(
+                IRanges::LogicalList(c(
+                  lapply(other_metadata, function(y) # matching the metadata
+                    r_data[,y] == drug_pairs[idp,y])
+                  ))), 2, all)
+
+    # reverse engineer the type of combination experiment
+    flat_data = gDR::assay_to_df(SE[row_idx, ], 'Averaged')
+    flat_data = flat_data[flat_data$Concentration_2 > 0,]
+    conc_1 = table(flat_data$Concentration)
+    conc_2 = table(flat_data$Concentration_2)
+    n_conc_pairs = nrow(unique(flat_data[,c('Concentration', 'Concentration_2')]))
+    conc_ratio = table(round(log10(flat_data$Concentration / flat_data$Concentration_2),2))
+    conc_ratio = conc_ratio[!names(conc_ratio) %in% c('Inf', '-Inf')]
+
+    condition = paste(paste(other_metadata, unlist(drug_pairs[idp,other_metadata]), sep = '='),
+                  collapse=' ')
+    if (length(conc_ratio) <= 2) {
+      type = 'co-dilution'
+      print(sprintf('Found %s combination with %s and %s: ratio of %.2f, %i concentrations (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], 10**as.numeric(names(conc_ratio)),
+            length(conc_2), condition))
+    } else if (n_conc_pairs == length(conc_1)*length(conc_2) & length(conc_2) >= 4) {
+      type = 'matrix'
+      print(sprintf('Found %s combination with %s and %s: %i x %i concentrations (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], length(conc_1), length(conc_2), condition))
+    } else if (length(conc_2)<4) {
+      type = 'fixed'
+      print(sprintf('Found %s combination of %s with %s at %.3g uM (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], as.numeric(names(conc_2)), condition))
+    } else {
+      type = 'other'
+      print(sprintf('Found %s combination with %s and %s: %i concentration pairs (%s)',
+        type, drug_pairs[idp,1], drug_pairs[idp,2], n_conc_pairs, condition))
+    }
+
+    pair_list[[idp]] = list(Gnumbers = unlist(drug_pairs[idp,]),
+                          rows = rownames(r_data)[row_idx],
+                          type = type)
+  }
+
+  metadata(SE)$drug_combinations = pair_list
+  return(SE)
+}
+
+      # for (iCL in 1:ncol(SE)) {
+      #     flat_data = assay_to_df(SE[row_idx, iCL], 'Averaged')
+      #     tail(unique(flat_data[, c('Gnumber', 'Concentration', 'Gnumber_2', 'Concentration_2')]),30)
+      # }
