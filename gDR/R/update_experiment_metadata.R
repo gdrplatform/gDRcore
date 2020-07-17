156e44f7d7ae31fa567a89610c0f813e56c709a2
bczech <rstudio@3c6009c96f49>
2020-07-17 08:54:22 +0000

    Update experiment_metadata

---
+++ gDR/R/update_experiment_metadata.R
@@ -0,0 +1,44 @@
+#' update_metadata
+#'
+#' Update metadata in a SummarizedExperiment
+#'
+#' @param SE a SummarizedExperiment
+#'
+#' @return a SummarizedExperiment with additional metadata
+#' @export
+#'
+
+update_experiment_metadata = function(
+                           metadata,
+                           metadataList) {
+  # Assertions:
+  checkmate::assert_list(metadata)
+  if(length(metadata$experiment_metadata)>0) {
+    description <- as.character(metadata$experiment_metadata$description)
+    experiment_name <- as.character(metadata$experiment_metadata$name)
+    expert_unixid <- as.character(metadata$experiment_metadata$experimentalist)
+    qcs_id <- as.character(strsplit(as.character(metadata$experiment_metadata$name), " ")[[1]][1])
+  }
+  checkmate::assert_true(all(names(metadataList) %in% c("expert_unixid", "description",
+                                                    "assay_id", "date_processed",
+                                                    "source_id", "state_id",
+                                                    "experiment_name", "qcs_id",
+                                                    "labhead_unixid")))
+  for (element in names(lista2)){
+    assign(element, lista2[[element]])
+  }
+  
+  metadata$experiment_metadata <- data.frame(
+    expert_unixid = ifelse(exists("expert_unixid"), expert_unixid, Sys.getenv("USER")),
+    description = ifelse(exists("description"), description, NA),
+    assay_id = ifelse(exists("assay_id"), assay_id, NA),
+    date_processed = ifelse(exists("date_processed"), date_processed, NA),
+    source_id = ifelse(exists("source_id"), source_id, NA),
+    state_id = ifelse(exists("state_id"), state_id, NA),
+    experiment_name = ifelse(exists("experiment_name"), experiment_name, NA),
+    qcs_id = ifelse(exists("qcs_id"), qcs_id, NA),
+    labhead_unixid = ifelse(exists("labhead_unixid"), labhead_unixid, NA)
+  )
+  return(metadata)
+}
+
