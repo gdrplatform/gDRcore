1a723ea981a643d4fa5b2275271121746185d43c
bczech <rstudio@3c6009c96f49>
2020-08-19 13:08:50 +0000

    Update experiment_metadata

--- gDR/R/update_experiment_metadata.R
+++ gDR/R/update_experiment_metadata.R
@@ -32,15 +32,36 @@ update_experiment_metadata = function(
   }
   }
   
-  metadata$experiment_metadata <- data.frame(
+  if (!exists('source_id')) {
+    source_id <-
+      gDRwrapper::get_source_keys() %>% dplyr::filter(source_type == "UNKNOWN") %>% dplyr::pull(source_id)
+  }
+  
+  if (!exists('assay_id')) {
+    assay_id <-
+      gDRwrapper::get_assay_types() %>% dplyr::filter(assay_type == "UNKNOWN") %>% dplyr::pull(assay_id)
+  }
+  
+  if (!exists('state_id')) {
+    state_id <-
+      tryCatch({
+        gDRwrapper::get_state_types() %>% dplyr::filter(state_name == "Active") %>% dplyr::pull(state_id)
+      },
+      error=function(cond) {
+        return(1)
+      })
+  }
+  
+  metadata$experiment_metadata <- tibble::tibble(
     expert_unixid = ifelse(exists("expert_unixid"), expert_unixid, Sys.getenv("USER")),
     unix_id = ifelse(exists("unix_id"), unix_id, Sys.getenv("USER")),
     description = ifelse(exists("description"), description, NA),
     assay_id = ifelse(exists("assay_id"), assay_id, NA),
-    date_processed = ifelse(exists("date_processed"), date_processed, NA),
+    date_experiment = ifelse(exists("date_experiment"), date_experiment, as.character(Sys.Date())),
     source_id = ifelse(exists("source_id"), source_id, NA),
     state_id = ifelse(exists("state_id"), state_id, NA),
     experiment_name = ifelse(exists("experiment_name"), experiment_name, NA),
+    experiment_project = ifelse(exists("experiment_project"), experiment_project, NA),
     qcs_id = ifelse(exists("qcs_id"), qcs_id, NA),
     labhead_unixid = ifelse(exists("labhead_unixid"), labhead_unixid, NA),
     stringsAsFactors = FALSE
