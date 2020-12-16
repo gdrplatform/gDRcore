a21bdc61fa2d2f61508dfe7f345bf472e40ff017
vuonga1 <vuong.allison@gene.com>
2020-12-15 18:47:31 -0800

    separate out functions into smaller files

--- gDR/R/average_SE.R
+++ gDR/R/average_SE.R
@@ -108,6 +108,76 @@ merge_data <- function(manifest, treatments, data) {
   return(df_raw_data)
 }
 
+#' average_SE
+#'
+#' Avereage normalized SummarizedExperiment of DR data
+#'
+#' @param normSE a SummarizedExperiment with normalized DR data
+#' @param TrtKeys a vector of keys used for averaging (NULL by default)
+#'
+#' @return a SummarizedExperiment with additional assay with averaged DR data
+#' @export
+#'
+
+average_SE <- function(normSE, TrtKeys = NULL, include_masked = F) {
+
+  # Assertions:
+  checkmate::assert_class(normSE, "SummarizedExperiment")
+  checkmate::assert_vector(TrtKeys, null.ok = TRUE)
+
+
+    avgSE <- normSE
+    if (is.null(TrtKeys)) {
+        if ("Keys" %in% names(metadata(normSE))) {
+          TrtKeys <- metadata(normSE)$Keys$Trt
+          TrtKeys <- setdiff(TrtKeys, metadata(normSE)$Keys$discard_keys)
+        } else {
+          TrtKeys <- identify_keys(normSE)$Trt
+        }
+    }
+    metadata(normSE)$Keys$Trt <- TrtKeys
+
+    SummarizedExperiment::assay(avgSE, "Averaged") <- SummarizedExperiment::assay(avgSE, "Normalized")
+    avgSE <- aapply(avgSE, function(x) {
+        # bypass 'masked' filter
+        x$masked = x$masked & !include_masked
+
+        subKeys <- intersect(TrtKeys, colnames(x))
+        if (sum(!x$masked) >= 1) {
+            df_av <- aggregate(x[ !x$masked ,
+                                  c("GRvalue", "RelativeViability","CorrectedReadout")],
+                            by = as.list(x[ !x$masked , subKeys, drop = FALSE]),
+                            FUN = function(y) mean(y, na.rm = TRUE))
+            df_std <- aggregate(x[!x$masked, c("GRvalue", "RelativeViability")],
+                                by = as.list(x[ !x$masked, subKeys, drop = FALSE]),
+                                FUN = function(x) sd(x, na.rm = TRUE))
+            colnames(df_std)[colnames(df_std) %in% c("GRvalue", "RelativeViability")] =
+                paste0("std_",
+                    colnames(df_std)[colnames(df_std) %in% c("GRvalue", "RelativeViability")])
+            return( merge(df_av, df_std, by = subKeys) )
+        } else { # case: (nrow(x) == 0 || all(x$masked))
+            df_ = as.data.frame(matrix(0,0,length(subKeys)+5))
+            colnames(df_) = c(subKeys,
+                  c("GRvalue", "RelativeViability","CorrectedReadout"),
+                  paste0("std_", c("GRvalue", "RelativeViability")))
+            return(df_)
+        } 
+    }, "Averaged")
+
+    SummarizedExperiment::assay(avgSE, "Avg_Controls") <- SummarizedExperiment::assay(avgSE, "Controls")
+    avgSE <- aapply(avgSE, function(x) {
+        if (nrow(x) > 1) {
+            subKeys <- intersect(TrtKeys, colnames(x))
+            df_av <- DataFrame(lapply(x[, c("Day0Readout", "UntrtReadout",
+                    "RefGRvalue", "RefRelativeViability",
+                    "RefReadout", "DivisionTime")], FUN = function(y) mean(y, na.rm = TRUE)))
+            return( df_av )
+        } else return(x)
+    }, "Avg_Controls")
+
+    return(avgSE)
+}
+
 #' metrics_SE
 #'
 #' Calculate metrics for DR data
@@ -364,9 +434,81 @@ Order_result_df <- function (df_) {
   return(df_)
 }
 
-standardize_record_values <- function(x, dictionary = DICTIONARY){
-  for (i in 1:length(dictionary)) {
-    x[x == names(dictionary[i])] <- dictionary[[i]]
+
+#' Add codrug group
+#'
+#' @param SE 
+#'
+#' @return
+#' @export
+#'
+add_codrug_group = function(SE) {
+
+  r_data = SummarizedExperiment::rowData(SE)
+  if (!(paste0(gDRutils::get_identifier()$drugname, '_2') %in% colnames(r_data))) return(SE)
+
+  # find the pairs of drugs with relevant metadata
+  drug_ids = paste0(gDRutils::get_identifier()$drugname, c('', '_2'))
+  other_metadata = c(paste0(gDRutils::get_identifier()$drug, c('', '_2')),
+            setdiff(colnames(r_data), c('Concentration_2', drug_ids,
+                paste0(gDRutils::get_identifier()$drug, c('', '_2')))))
+  drug_pairs = unique(r_data[, c(drug_ids, other_metadata)])
+  drug_pairs = drug_pairs[ !(drug_pairs[,drug_ids[2]] %in% gDRutils::get_identifier('untreated_tag')),]
+
+  pair_list = vector('list', nrow(drug_pairs))
+  # loop through the pairs to assess the number of individual concentration pairs
+  for (idp in 1:nrow(drug_pairs)) {
+    row_idx = r_data[,drug_ids[1]] %in% unlist(drug_pairs[idp, drug_ids]) &
+            r_data[,drug_ids[2]] %in% c(unlist(drug_pairs[idp, drug_ids]),
+                gDRutils::get_identifier('untreated_tag')) &
+            apply(as.matrix(
+                IRanges::LogicalList(c(
+                  lapply(setdiff(other_metadata,
+                      paste0(gDRutils::get_identifier()$drug, c('', '_2'))),
+                    function(y) # matching the metadata
+                    r_data[,y] == drug_pairs[idp,y])
+                  ))), 2, all)
+
+    # reverse engineer the type of combination experiment
+    flat_data = gDRutils::assay_to_df(SE[row_idx, ], 'Averaged')
+    flat_data = flat_data[flat_data$Concentration_2 > 0,]
+    conc_1 = table(flat_data$Concentration)
+    conc_2 = table(flat_data$Concentration_2)
+    n_conc_pairs = nrow(unique(flat_data[,c('Concentration', 'Concentration_2')]))
+    conc_ratio = table(round(log10(flat_data$Concentration / flat_data$Concentration_2),2))
+    conc_ratio = conc_ratio[!names(conc_ratio) %in% c('Inf', '-Inf')]
+
+    condition = paste(paste(other_metadata, unlist(drug_pairs[idp,other_metadata]), sep = '='),
+                  collapse=' ')
+    if (length(conc_ratio) <= 2) {
+      type = 'co-dilution'
+      print(sprintf('Found %s combination with %s and %s: ratio of %.2f, %i concentrations (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], 10**as.numeric(names(conc_ratio)),
+            length(conc_2), condition))
+    } else if (n_conc_pairs == length(conc_1)*length(conc_2) & length(conc_2) >= 4) {
+      type = 'matrix'
+      print(sprintf('Found %s combination with %s and %s: %i x %i concentrations (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], length(conc_1), length(conc_2), condition))
+    } else if (length(conc_2)<4) {
+      type = 'fixed'
+      print(sprintf('Found %s combination of %s with %s at %.3g uM (%s)',
+          type, drug_pairs[idp,1], drug_pairs[idp,2], as.numeric(names(conc_2)), condition))
+    } else {
+      type = 'other'
+      print(sprintf('Found %s combination with %s and %s: %i concentration pairs (%s)',
+        type, drug_pairs[idp,1], drug_pairs[idp,2], n_conc_pairs, condition))
+    }
+
+    pair_list[[idp]] = list(condition = unlist(drug_pairs[idp,]),
+                          rows = rownames(r_data)[row_idx],
+                          type = type)
   }
-  x
+
+  metadata(SE)$drug_combinations = pair_list
+  return(SE)
 }
+
+      # for (iCL in 1:ncol(SE)) {
+      #     flat_data = assay_to_df(SE[row_idx, iCL], 'Averaged')
+      #     tail(unique(flat_data[, c('Gnumber', 'Concentration', 'Gnumber_2', 'Concentration_2')]),30)
+      # }
