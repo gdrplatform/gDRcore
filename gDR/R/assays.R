a8381c42153b95dec6a558601798b86d9ceb5280
vuonga1 <vuong.allison@gene.com>
2021-01-31 17:02:53 -0800

    add function and tests to assign treated and untreated conditions

--- gDR/R/assays.R
+++ gDR/R/assays.R
@@ -15,6 +15,27 @@
     "df_metrics")
 
 
+#' .assign_treated_and_untreated_conditions
+#'
+#' Assign the treated and untreated conditions to a data.frame
+#'
+#' @param data.frame containing the drug name field.
+#'
+#' @return data.frame containing an additional factor column called \code{treated_untreated} specifying
+#' whether the condition is a treated or untreated entry.
+#'
+#' @export
+#'
+.assign_treated_and_untreated_conditions <- function(df_) {
+  drugnames <- tolower(as.data.frame(df_)[, gDRutils::get_identifier("drugname")])
+  untreated <- grepl(.untreatedDrugNameRegex, drugnames)
+  df_$treated_untreated <- "treated"
+  df_$treated_untreated[untreated] <- "untreated"
+  df_$treated_untreated <- as.factor(df_$treated_untreated)
+  df_
+}
+
+
 #' .get_untreated_conditions
 #'
 #' Get untreated conditions
@@ -71,59 +92,4 @@ aapply <-
 
     SummarizedExperiment::assay(SE, assay_type, withDimnames=FALSE) = matrix(sapply(SummarizedExperiment::assay(SE, assay_type), fx), nrow = nrow(SE), ncol = ncol(SE))
     return(SE)
-  }
-
-
-#' addAssayToMAE
-#'
-#' Add assay to one of MAEs experiments (i.e. SEs) with dose-reponse data
-#'
-#' @param mae  MultiAssayExperiment object with dose-reponse
-#' @param assay matrix with dose-response data
-#' @param exp_name string name of the MAE experiment (i.e. SEs) to which add the assay
-#' @param assay_name string name of the assay to be used in SE
-#' @param update_assay logical allow for assay update if the assay with 'assay_name' currently exists in given 'exp_name'
-#'
-#' @return MultiAssayExperiment object with dose-reponse data
-#'
-#' @export
-addAssayToMAE <-
-  function(mae,
-           assay,
-           assay_name,
-           exp_name = c("treated", "untreated"),
-           update_assay = FALSE) {
-    # Assertions:
-    stopifnot("MultiAssayExperiment" %in% class(mae))
-    stopifnot(assay_name %in% .assayNames)
-    stopifnot("matrix" %in% class(assay))
-    checkmate::assert_logical(update_assay)
-
-    exp_name <- match.arg(exp_name)
-    #mae must contain SE with at least first assay (i.e. df_raw_data)
-    stopifnot(.assayNames[1] %in% SummarizedExperiment::assayNames(mae[[exp_name]]))
-
-    if (assay_name %in% SummarizedExperiment::assayNames(mae[[exp_name]]) &&
-        update_assay == FALSE) {
-      futile.logger::flog.error(
-        "The assay '%s' can't be added to experiment '%s' as it currently exists.
-        Please set 'update_assay' flag to TRUE to be able to update the assay instead of adding it",
-        assay_name,
-        exp_name
-      )
-      stop()
-    }
-
-    if (!identical(dim(SummarizedExperiment::assay(mae[[exp_name]])), dim(assay))) {
-      futile.logger::flog.error(
-        "The assay '%s' can't be added to experiment '%s' as it has different dimensions ('%s') than the assays present in the experiment ('%s').",
-        assay_name,
-        exp_name,
-        paste(dim(assay), collapse = "x"),
-        paste(dim(SummarizedExperiment::assay(mae[[exp_name]])), collapse = "x")
-      )
-      stop()
-    }
-
-    SummarizedExperiment::assay(mae[[exp_name]], assay_name) <- assay
   }
--- gDR/tests/testthat/test-assays.R
+++ gDR/tests/testthat/test-assays.R
@@ -19,3 +19,12 @@ test_that(".get_untreated_conditions works", {
   expect_equal(untrt, data$name_[seq_len(n_untrt)])
   expect_equal(trt, data$name_[setdiff(seq_len(length(data)), seq_len(n_untrt))])
 })
+
+
+test_that(".assign_treated_and_untreated_conditions works", {
+  drugnames <- c(paste0("G", seq_len(5)), rep(gDRutils::get_identifier("untreated_tag"), 2))
+  df <- data.frame(DrugName = drugnames) 
+  df <- .assign_treated_and_untreated_conditions(df)
+  expect_true(all(dim(df) == c(9, 2)))
+  expect_equal(df$treated_untreated, as.factor(c(rep("treated", 5), rep("untreated", 4))))
+})
