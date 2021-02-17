e26d77cd7b927c22a20274175e4c59983c78ef69
bczech <bartosz.czech@contractors.roche.com>
2021-02-17 14:33:35 +0100

    Update drug annotation function

--- gDR/R/add_annotation.R
+++ gDR/R/add_annotation.R
@@ -141,7 +141,7 @@ add_Drug_annotation <- function(df_metadata,
         Drug_info <- Drug_info[!duplicated(Drug_info[["drug"]]),]
         DrIDs <- unique(unlist(df_metadata[,grep(gDRutils::get_identifier("drug"), colnames(df_metadata)), with = FALSE]))
         if(any(!gsub("\\..*", "", drugsTreated) %in% Drug_info$drug) & exists("missingTblDrugs")){
-          Drug_info <- rbind(Drug_info, data.table::setnames(missingTblDrugs[!drugsTreated %in% Drug_info$drug, c(3,1)], names(Drug_info)))
+          Drug_info <- rbind(Drug_info, data.table::setnames(missingTblDrugs[!drugsTreated %in% Drug_info$drug, ], names(Drug_info)))
         }
         bad_DrID <- !(gsub("\\..*", "", DrIDs) %in% Drug_info$drug) & !is.na(DrIDs)
         if (any(bad_DrID)) {
