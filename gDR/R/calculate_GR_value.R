f8cbcb0843864201a22c6e9c7452e2f07ae3c3ac
vuonga1 <vuong.allison@gene.com>
2021-02-22 09:20:37 -0800

    improve calculate_GR_value

--- gDR/R/calculate_GR_value.R
+++ gDR/R/calculate_GR_value.R
@@ -2,35 +2,33 @@
 #'
 #' Calculate a GR value for a given set of dose response values.
 #'
-#' @param df_ data.frame of dose response values containing \code{"CorrectedReadout"}.
-#' @param cellline_md named list of metadata for the cell line of interest.
-#' @param day0_readout numeric vector 
-#' @param ndigit_rounding integer
+#' @param df_ data.frame of dose response values containing columns named
+#' \code{"CorrectedReadout"}, \code{"Day0Readout"}, and \code{"UntrtReadout"}.
+#' @param duration numeric value specifying the length of time the cells were treated.
+#' @param ref_div_time numeric value specifying the reference division time for the cell line of interest.
+#' @param cl_name character string specifying the name for the cell line of interest.
+#' @param ndigit_rounding integer specifying the number of digits to use for calculation rounding.
 #'
 #' @return 
 #'
 #' @export
 #'
-calculate_GR_value <- function(df_, cellline_md, day0_readout, ndigit_rounding) {
-  if (any(is.na(day0_readout))) {
+calculate_GR_value <- function(df_, ndigit_rounding, duration, ref_div_time, cl_name) {
+  if (any(is.na(df_$Day0_readout))) {
     ## Back-calculate the day0_readout using the reference doubling time and the duration of treatment.
-    # TODO: These column values should later be read directly from the SE itself. 
-    ref_div_time <- as.numeric(cellline_md[[gDRutils::get_identifier("cellline_ref_div_time")]])
-    cl_name <- cellline_md[[gDRutils::get_identifier("cellline_name")]]
-    duration <- cellline_md[[gDRutils::get_identifier("duration")]]
-
     if (is.null(ref_div_time) || is.na(ref_div_time)) {
-      futile.logger::flog.warn(paste("no day 0, no reference doubling time, so GR values are NA for cell line", cl_name))
+      futile.logger::flog.warn(sprintf("no day 0, no reference doubling time, so GR values are NA for cell line '%s'", cl_name))
     } else if (ref_div_time > 1.5 * duration) {
-      futile.logger::flog.warn(sprintf("reference doubling time for cell line '%s'='%s', too long for GR calculation with assay duration ('%s'), setting GR values to NA", 
-        cl_name, ref_div_time, duration))
+      futile.logger::flog.warn(
+        sprintf("reference doubling time for cell line '%s'='%s', too long for GR calculation with assay duration ('%s'), setting GR values to NA", 
+          cl_name, ref_div_time, duration))
     } else {
-      futile.logger::flog.warn(paste("no day 0, calculating GR value based on reference doubling time for ", cl_name))
+      futile.logger::flog.warn(sprintf("no day 0 data, calculating GR value based on reference doubling time for '%s'", cl_name))
     }
 
-    GRvalue <- calculate_endpt_GR_value(df_, duration, ref_div_time, ndigit_rounding = nDigits_rounding)
+    GRvalue <- calculate_endpt_GR_value(df_, duration, ref_div_time, ndigit_rounding = ndigit_rounding)
   } else {
-    GRvalue <- calculate_time_dep_GR_value(df_$CorrectedReadout, df_$Day0Readout, df_$UntrtReadout, nDigits_rounding)
+    GRvalue <- calculate_time_dep_GR_value(df_$CorrectedReadout, df_$Day0Readout, df_$UntrtReadout, ndigit_rounding)
   }
   GRvalue
 }
