a06ef476bef1dae8bbd46b3b8bb58c3c3979bf8c
vuonga1 <vuong.allison@gene.com>
2021-03-02 09:28:48 -0800

    update runDrugResponseProcessingPipeline to also support conveninece function for old method

--- gDR/R/runDrugResponseProcessingPipeline.R
+++ gDR/R/runDrugResponseProcessingPipeline.R
@@ -1,7 +1,7 @@
 # TODO: Add in all other parameters. 
 #' @export
 #'
-runDrugResponseProcessingPipeline <- function(df_) {
+runDrugResponseProcessingPipeline2 <- function(df_) {
   se <- create_SE2(df_)
   se <- normalize_SE2(se)
   se <- average_SE2(se)
@@ -9,9 +9,10 @@ runDrugResponseProcessingPipeline <- function(df_) {
   se
 }
 
+
 #' @export
 #'
-runDrugResponseProcessingPipeline1 <- function(df_) {
+runDrugResponseProcessingPipeline <- function(df_) {
   se <- normalize_SE(df_)
   se <- average_SE(se)
   se <- fit_SE(se)
