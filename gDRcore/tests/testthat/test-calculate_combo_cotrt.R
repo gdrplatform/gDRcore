0e9bd10f13da294ae4326f3fb85a0db969446be4
Mocanu, Sergiu {FIEC~Mississauga} <sergiu.mocanu@contractors.roche.com>
2021-07-08 12:16:40 -0400

    update according to review

--- gDRcore/tests/testthat/test-calculate_combo_cotrt.R
+++ gDRcore/tests/testthat/test-calculate_combo_cotrt.R
@@ -1,14 +1,12 @@
-library(testthat)
-library(gDRcore)
-se <- readRDS(system.file("testdata", 
-                          "finalSE_combo_codilution_small.RDS", 
-                          package = "gDRtestData"))
 
-n <- 4
-drug_names <- paste0("drug_00", seq(n))
-printed_output <- cat(paste0("[1] \"", drug_names[2:4], " x ", drug_names[1], " (", 
-                             seq_along(drug_names[2:4]), "/", length(drug_names[2:4]), 
-                             ")\"\n"), 
-                      sep = "")
-test_that("check printed output",
-          expect_output(calculate_combo_cotrt(se), printed_output))
+test_that("check printed output", {
+  se <- get_synthetic_data("finalSE_combo_codilution_small.RDS")
+  
+  n <- 4
+  drug_names <- paste0("drug_00", seq(n))
+  printed_output <- cat(paste0("[1] \"", drug_names[2:4], " x ", drug_names[1], " (", 
+                               seq_along(drug_names[2:4]), "/", length(drug_names[2:4]), 
+                               ")\"\n"), 
+                        sep = "")
+  expect_output(calculate_combo_cotrt(se), printed_output)
+})
--- gDRcore/tests/testthat/test-calculate_combo_matrix.R
+++ gDRcore/tests/testthat/test-calculate_combo_matrix.R
@@ -1,14 +1,11 @@
-library(testthat)
-library(gDRcore)
-se <- readRDS(system.file("testdata", 
-                          "finalSE_combo_matrix_small.RDS", 
-                          package = "gDRtestData"))
 
-cell_line_names <- rep(c("cellLine_GB", "cellline_HB"), 12)
-drug_names1 <- rep(rep(paste0("drug_00", seq(4, 6)), each = 4), 2)
-drug_names2 <- rep(rep(c("drug_021", "drug_026"), each = 2), 6)
-printed_output <- cat(paste0("[1] \"Calculation for cell line ", cell_line_names,
-                             " treated with ", drug_names1,
-                             " x ", drug_names2, "\"\n"), sep = "")
-test_that("check printed output",
-          expect_output(suppressWarnings(calculate_combo_matrix(se)), printed_output))
+test_that("check printed output", {
+  se <- get_synthetic_data("finalSE_combo_matrix_small.RDS")
+  cell_line_names <- rep(c("cellLine_GB", "cellline_HB"), 12)
+  drug_names1 <- rep(rep(paste0("drug_00", seq(4, 6)), each = 4), 2)
+  drug_names2 <- rep(rep(c("drug_021", "drug_026"), each = 2), 6)
+  printed_output <- cat(paste0("[1] \"Calculation for cell line ", cell_line_names,
+                               " treated with ", drug_names1,
+                               " x ", drug_names2, "\"\n"), sep = "")
+  expect_output(suppressWarnings(calculate_combo_matrix(se)), printed_output)
+})
