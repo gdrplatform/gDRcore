ec610c8b383b1f42c40ea8b5abd0a3b8cc200f4b
allisonvuong <allison.vuong@gmail.com>
2021-08-10 17:54:21 -0700

    feat: cotreatment fittings for combo data

--- gDRcore/R/cotreatments.R
+++ gDRcore/R/cotreatments.R
@@ -1,6 +1,5 @@
 #' @keywords internal
-fit_cotreatments <- function(measured, nested_identifiers, normalization_type) {       
-  # Identify the combo concentrations.
+fit_combo_cotreatments <- function(measured, nested_identifiers, normalization_type) {       
   id <- nested_identifiers[1] # (i.e. conc1)
   id2 <- nested_identifiers[2] # (i.e. conc2)
 
@@ -8,58 +7,50 @@ fit_cotreatments <- function(measured, nested_identifiers, normalization_type) {
   all_conc2 <- setdiff(unique(measured[, id2]), 0)
 
   # Single agent fits.
-  single_agent1 <- fit_cotreatment_series(measured, series_id = id, cotrt_id = id2, cotrt_value = 0,
-    normalization_type, all_conc = all_conc1)
-  single_agent2 <- fit_cotreatment_series(measured, series_id = id2, cotrt_id = id, cotrt_value = 0,
-    normalization_type, all_conc = all_conc2)
+  sa_fit1 <- fit_cotreatment_series(measured, series_id = id, cotrt_id = id2, cotrt_value = 0,
+    normalization_type = normalization_type, e_0 = 1, GR_0 = 1)
+  sa_fit2 <- fit_cotreatment_series(measured, series_id = id2, cotrt_id = id, cotrt_value = 0,
+    normalization_type = normalization_type, e_0 = 1, GR_0 = 1)
 
   # Fit cotreatments in matrix.
   cotrt_fittings <- vector("list", sum(length(all_conc1), length(all_conc2)))
   n <- 1
   for (conc1 in all_conc1) {
+    # TODO: Switch to ec50 once c50 -> ec50
+    single_agent2 <- gDRutils::logistic_4parameters(conc1, sa_fit1$x_inf, sa_fit1$x_0, sa_fit1$c50, sa_fit1$h)
     cotrt_fittings[[n]] <- fit_cotreatment_series(measured, series_id = id2, cotrt_id = id,
-      cotrt_value = conc1, all_conc = all_conc2, e_0 = single_agent2, GR_0 = single_agent2)
+      cotrt_value = conc1, normalization_type = normalization_type, e_0 = single_agent2, GR_0 = single_agent2)
     n <- n + 1
   }
   for (conc2 in all_conc2) {
+    single_agent1 <- gDRutils::logistic_4parameters(conc2, sa_fit2$x_inf, sa_fit2$x_0, sa_fit2$c50, sa_fit2$h)
     cotrt_fittings[[n]] <- fit_cotreatment_series(measured, series_id = id, cotrt_id = id2,
-      cotrt_value = conc2, all_conc = all_conc1, e_0 = single_agent1, GR_0 = single_agent1)
+      cotrt_value = conc2, normalization_type = normalization_type, e_0 = single_agent1, GR_0 = single_agent1)
     n <- n + 1
   }
 
-  cotrt_fittings
+  do.call("rbind", cotrt_fittings)
 }
 
 
 #' @keywords internal
-fit_cotreatment_series <- function(flat_data, series_id, cotrt_id, cotrt_value, normalization_type, all_conc = NULL) {
+fit_cotreatment_series <- function(measured, series_id, cotrt_id, cotrt_value, normalization_type, e_0, GR_0) {
   assert_numeric(cotrt_value)
-  df_ <- flat_data[flat_data[[cotrt_id]] == cotrt_value & flat_data[[series_id]] > 0, , drop = FALSE]
+  df_ <- measured[measured[[cotrt_id]] == cotrt_value & measured[[series_id]] > 0, , drop = FALSE]
   cotrt_fit <- gDRutils::fit_curves(
-    df_ = df_[!is.na(df_[, normalization_type]), , drop = FALSE],
+    df_ = df_,
     series_identifiers = series_id,
+    e_0 = e_0,
+    GR_0 = GR_0,
     force_fit = TRUE,
     cap = 0.2,
     normalization_type = normalization_type
   )
 
-  # Fill any missing concentration readouts.
-  if (any(missing_concs <- setdiff(all_conc, unique(df_[[series_id]])))) {
-    vals <- gDRutils::logistic_4parameters(
-      missing_concs, 
-      cotrt_fit$x_inf, 
-      cotrt_fit$x_0, 
-      cotrt_fit$ec50,
-      cotrt_fit$h)
+  nested_df <- DataFrame()
+  nested_df[series_id] <- df_[[series_id]]
+  nested_df[cotrt_id] <- cotrt_value
 
-    df_inf <- data.frame(Concentration = missing_concs)
-    df_inf[[norm_method]] <- vals
-    missing_cols <- grepl("std_", colnames(df_))
-    missing_cols <- c(missing_cols, colnames(df_) %in% c("CorrectedReadout", setdiff(c("RelativeViability", "GRvalue"), norm_method)))
-    df_inf[missing_cols] <- 0 
-    cotrt_fit <- rbind(cotrt_fit, df_inf)
-  }
-
-  # TODO: Include 2 Iranges columns of the concentration columns.
-  cotrt_fit
+  out <- DataFrame(IRanges::DataFrameList(nested_df), cotrt_fit)
+  out
 }
---
+++ gDRcore/tests/testthat/test-cotreatments.R
@@ -0,0 +1,43 @@
+test_that("fit_combo_cotreatments works as expected", {
+  n <- 10
+  concs <- seq(0, n, 1)
+  start <- gDRutils::logistic_4parameters(concs, Vinf = 0.1, V0 = 1, EC50 = 0.5, h = 2)
+  vals  <- NULL
+  for (i in seq(n)) {
+    vals <- c(vals, start + i*concs)
+  }
+  nested_identifiers <- c("Concentration", "Concentration_2")
+  measured <- DataFrame(Concentration = rep(concs, n),
+    Concentration_2 = rep(concs, each = n),
+    GRvalue = vals)
+
+  obs <- gDRcore:::fit_combo_cotreatments(measured, nested_identifiers, "GR")
+  expect_equal(dim(obs), c(20, 18))
+  expect_true(nested_identifiers %in% colnames(obs))
+  expect_equal(unname(vapply(obs[, nested_identifiers], class, "")), c("SimpleNumericList", "SimpleNumericList"))
+})
+
+
+test_that("fit_cotreatment_series works as expected", {
+  n <- 10
+  concs <- seq(1, n, 1)
+  start <- gDRutils::logistic_4parameters(concs, Vinf = 0.1, V0 = 1, EC50 = 0.5, h = 2)
+  vals  <- NULL
+  for (i in seq(n)) {
+    vals <- c(vals, start + i*concs)
+  }
+  nested_identifiers <- c("Concentration", "Concentration_2")
+  measured <- DataFrame(Concentration = rep(concs, n),
+    Concentration_2 = rep(concs, each = n),
+    GRvalue = vals)
+
+  obs <- gDRcore:::fit_cotreatment_series(measured, 
+    series_id = "Concentration",
+    cotrt_id = "Concentration_2",
+    cotrt_value = 1,
+    normalization_type = "GR")
+
+  expect_equal(dim(obs), c(1, 18))
+  expect_true(nested_identifiers %in% colnames(obs))
+  expect_equal(unname(vapply(obs[, nested_identifiers], class, "")), c("SimpleNumericList", "SimpleNumericList"))
+})
