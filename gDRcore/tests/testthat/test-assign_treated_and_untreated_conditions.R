a7fa4fb1bd9c7868e4131f2cf2160c51243aecec
allisonvuong <allison.vuong@gmail.com>
2021-08-18 17:01:39 -0700

    test: extend assign_treated_and_untreated_conditions tests to cover combination screens

--- gDRcore/tests/testthat/test-assign_treated_and_untreated_conditions.R
+++ gDRcore/tests/testthat/test-assign_treated_and_untreated_conditions.R
@@ -4,6 +4,12 @@ test_that(".assign_treated_and_untreated_conditions works", {
   df <- .assign_treated_and_untreated_conditions(df)
   expect_true(all(dim(df) == c(9, 2)))
   expect_equal(df$treated_untreated, as.factor(c(rep("treated", 5), rep("untreated", 4))))
+
+  # Multiple drugs.
+  df_ <- data.frame(DrugName = c("vehicle", "vehicle", "bogus"), DrugName2 = c("vehicle", "bogus", "bogus"))
+  df_ <- .assign_treated_and_untreated_conditions(df_, drugname_col = c("DrugName", "DrugName2"))
+  expect_true(all(dim(df_) == c(3, 3)))
+  expect_equal(df_$treated_untreated, as.factor("untreated", "treated", "treated"))
 })
 
 
