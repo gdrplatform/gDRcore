b3c672be9ed16721745002b35dda9e19d3ed7806
allisonvuong <allison.vuong@gmail.com>
2021-09-21 16:52:26 -0700

    test: add tests for calculate_excess

---
+++ gDRcore/tests/testthat/test-combinations.R
@@ -0,0 +1,17 @@
+library(testthat); library(gDRcore)
+
+test_that("calculate_excess works as expected", {
+  metric <- data.frame(Concentration = c(1, 2, 3, 1, 2, 3),
+    Concentration_2 = c(1, 1, 1, 2, 2, 2),
+    GRvalue = c(100, 200, 300, 400, 500, 600))
+  measured <- data.frame(Concentration = c(3, 1, 2, 2, 1, 3),
+    Concentration_2 = c(1, 1, 1, 2, 2, 2),
+    testvalue = c(200, 0, 100, 400, 300, 500))
+  series_identifiers <- c("Concentration", "Concentration_2")
+  metric_col <- "GRvalue"
+  measured_col <- "testvalue"
+  obs <- calculate_excess(metric, measured, series_identifiers, metric_col, measured_col)
+  
+  expect_equal(obs[, series_identifiers], measured[, series_identifiers])
+  expect_equal(obs$excess, c())
+})
