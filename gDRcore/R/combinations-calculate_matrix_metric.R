29d360ab1510d214729eee837d489e7c6054c287
vuonga1 <vuonga1@gene.com>
2021-10-21 09:53:12 -0700

    chore: reorganize combinations functions into more granular files

---
+++ gDRcore/R/combinations-calculate_matrix_metric.R
@@ -0,0 +1,59 @@
+#' Calculate a metric for combination data.
+#'
+#' Calculate a metric based off of single-agent values in combination screens.
+#'
+#' @param sa1 data.frame containing single agent data where id1 is \code{0}.
+#' Columns of the data.frame include identifiers and the \code{metric} of interest.
+#' @param series_id1 String representing the column within \code{sa1} that represents id1.
+#' @param sa2 data.frame containing single agent data where id2 is \code{0}.
+#' Columns of the data.frame include identifiers and the \code{metric} of interest.
+#' @param series_id2 String representing the column within \code{sa1} that represents id2.
+#' @param metric String of the column specifying the metric of interest. 
+#' @param FXN Function to apply to the single-agent fits to calculate a metric.
+#'
+#' @return DataFrame containing a single row for every unique combination of the two series
+#' identifiers and the corresponding calculated metric for each row.
+#'
+#' @name calculate_matrix_metric
+#' @details
+#' \code{calculate_HSA} takes the minimum of the two single agents readouts.
+#' \code{calculate_Bliss} performs Bliss additivity calculation based on the single agent effects,
+#' defined as \code{1-x} for the corresponding normalization.
+#' See https://www.sciencedirect.com/science/article/pii/S1359644619303460?via%3Dihub#tb0005
+#' for more details.
+NULL
+
+
+#' @rdname calculate_matrix_metric
+#' @export
+calculate_HSA <- function(sa1, series_id1, sa2, series_id2, metric) {
+  .calculate_matrix_metric(sa1, series_id1, sa2, series_id2, metric, FXN = pmin)
+}
+
+
+#' @rdname calculate_matrix_metric
+#' @export
+calculate_Bliss <- function(sa1, series_id1, sa2, series_id2, metric) {
+  lambda <- function(x, y) {
+    1 - (1 - x) - (1 - y) + (1 - x) * (1 - y)
+  }
+  .calculate_matrix_metric(sa1, series_id1, sa2, series_id2, metric, FXN = lambda)
+}
+
+
+#' @keywords internal
+.calculate_matrix_metric <- function(sa1, series_id1, sa2, series_id2, metric, FXN) {
+  colnames(sa1)[colnames(sa1) == metric] <- "metric1"
+  colnames(sa2)[colnames(sa2) == metric] <- "metric2"
+
+  # TODO: ensure they're unique?
+  u <- expand.grid(sa1[[series_id1]], sa2[[series_id2]])
+  colnames(u) <- c(series_id1, series_id2)
+
+  idx <- match(u[[series_id1]], sa1[[series_id1]])
+  u <- base::merge(u, sa1[, c(series_id1, "metric1")], by = series_id1)
+  u <- base::merge(u, sa2[, c(series_id2, "metric2")], by = series_id2)
+
+  metric <- do.call(FXN, list(u$metric1, u$metric2))
+  cbind(u, metric)
+}
