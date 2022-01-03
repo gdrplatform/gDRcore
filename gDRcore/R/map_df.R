6b0ce1e15c54553e7d3f4fb8656e5aac67cf2e9c
Bartosz Czech <bartosz.czech@contractors.roche.com>
2022-01-03 16:34:38 +0100

    refactor: speed-up map_df by using parallel computing

--- gDRcore/R/map_df.R
+++ gDRcore/R/map_df.R
@@ -32,15 +32,15 @@ map_df <- function(trt_md,
   checkmate::assert_character(ref_type)
   
   ref_type <- match.arg(ref_type)
-
+  
   duration_col <- gDRutils::get_env_identifiers("duration")
-
+  
   conc <- cbind(array(0, nrow(ref_md)), # padding to avoid empty df;
-    ref_md[, agrep("Concentration", colnames(ref_md)), drop = FALSE])
+                ref_md[, agrep("Concentration", colnames(ref_md)), drop = FALSE])
   is_ref_conc <- apply(conc, 1, function(z) {
     all(z == 0)
-    })
-
+  })
+  
   if (ref_type == "Day0") {
     # Identifying which of the durations have a value of 0.
     matching_list <- list(T0 = ref_md[, duration_col] == 0, conc = is_ref_conc)
@@ -49,28 +49,33 @@ map_df <- function(trt_md,
     matching_list <- list(conc = is_ref_conc)
     matchFactor <- duration_col 
   }
-
+  
   trt_rnames <- rownames(trt_md)
   ref_rnames <- rownames(ref_md)
-
+  
   # define matrix with matching metadata
   present_ref_cols <- intersect(ref_cols, names(ref_md))
   names(present_ref_cols) <- present_ref_cols
-
+  
   out <- list("vector", length(trt_rnames))
   msgs <- NULL
-  for (i in seq_len(length(trt_rnames))) {
+  
+  # Parallel computing
+  clusters <- parallel::makeCluster(cores, type = "FORK")
+  doParallel::registerDoParallel(clusters)
+  
+  out <- foreach::foreach(i = seq_along(trt_rnames)) %dopar% {
     treatment <- trt_rnames[i]
     refs <- lapply(present_ref_cols, function(y) {
       ref_md[, y] == trt_md[treatment, y]
-      })
-
+    })
+    
     if (!is.null(override_untrt_controls)) {
       for (overridden in names(override_untrt_controls)) {
         refs[[overridden]] <- ref_md[, overridden] == override_untrt_controls[[overridden]]
       }
     }
-
+    
     all_checks <- c(refs, matching_list)
     match_mx <- do.call("rbind", all_checks)
     rownames(match_mx) <- names(all_checks)
@@ -85,13 +90,15 @@ map_df <- function(trt_md,
       if (any(idx > 0)) {
         match_idx <- which.max(idx)
         msgs <- c(msgs, sprintf("Found partial match: ('%s') for treatment: ('%s')",
-          rownames(ref_md)[match_idx], treatment))
+                                rownames(ref_md)[match_idx], treatment))
       } else { # failed to find any potential match
         msgs <- c(msgs, sprintf("No partial match found for treatment: ('%s')", treatment))
       }
     }
-    out[[i]] <- ref_rnames[match_idx] # TODO: Check that this properly handles NAs or NULLs. 
+    ref_rnames[match_idx] # TODO: Check that this properly handles NAs or NULLs. 
   }
+  parallel::stopCluster(clusters)
+  
   futile.logger::flog.info(paste0(msgs, collapse = "\n"))
   names(out) <- trt_rnames
   out
