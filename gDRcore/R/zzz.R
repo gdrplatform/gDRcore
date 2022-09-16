5fdd39e19c913672a198eebaedb12610834663a5
czechb3 <bartosz.czech@contractors.roche.com>
2022-09-16 13:03:39 +0200

    refactor: set parameters for BiocParallel

--- gDRcore/R/zzz.R
+++ gDRcore/R/zzz.R
@@ -1,5 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   options(scipen = 999) # scientific notation was disabled due to the problem with unit tests
+  # set default options for BiocParallel
+  options(MulticoreParam = MulticoreParam(workers = max(1, multicoreWorkers() - 5)))
 }
 
 # data.table awareness
