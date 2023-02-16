01daa568c0d35974c62d225e9932597f361c81d0
Mocanu, Sergiu {FIEC~Mississauga} <sergiu.mocanu@contractors.roche.com>
2023-02-16 10:30:43 -0500

    chore: appease lintr

--- gDRcore/tests/testthat/test-merge_data.R
+++ gDRcore/tests/testthat/test-merge_data.R
@@ -3,8 +3,10 @@ library(testthat)
 test_that("merge_data works as expected", {
 
   manifestFile <- system.file(package = "gDRimport", "extdata", "data1", "manifest.xlsx")
-  templateFiles <- list.files(system.file(package = "gDRimport", "extdata", "data1"), pattern = "^Template", full.names = TRUE)
-  rawDataFiles <- list.files(system.file(package = "gDRimport", "extdata", "data1"), pattern = "^RawData", full.names = TRUE)
+  templateFiles <- list.files(system.file(package = "gDRimport", "extdata", "data1"),
+                              pattern = "^Template", full.names = TRUE)
+  rawDataFiles <- list.files(system.file(package = "gDRimport", "extdata", "data1"),
+                             pattern = "^RawData", full.names = TRUE)
 
   manifest <- gDRimport::load_manifest(manifestFile)
   template <- gDRimport::load_templates(templateFiles)
