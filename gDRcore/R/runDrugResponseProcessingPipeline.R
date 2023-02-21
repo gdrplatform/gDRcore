f4d0a043bb82afa50c81d8e89f35e23288afd9da
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-02-21 15:13:00 +0100

    refactor: add support for partial pipeline runs
    Details:
     - user can select the step from which the pipeline can be run
     - user can select experiments for which the pipeline while be run
     - added logic for saving/reading (per experiment/step) intermedia data
     - added helper functions
        * prepare_input
        * get_pipeline_steps
        * do_skip_test
        * is_preceding_step
        * save_intermediate_data
        * read_intermediate_data

--- gDRcore/R/runDrugResponseProcessingPipeline.R
+++ gDRcore/R/runDrugResponseProcessingPipeline.R
@@ -1,4 +1,4 @@
-#' Run drug response processing pipeline
+' Run drug response processing pipeline
 #'
 #' Run different components of the gDR drug response processing pipeline.
 #' Either: create a SummarizedExperiment and normalize raw treated and control data (create_and_normalize_SE),
@@ -43,6 +43,12 @@
 #' @param pcutoff numeric cutoff value.
 #' @param cap numeric value representing the value to cap the highest allowed relative viability at.
 #' @param curve_type vector of curve type values.
+#' @param data_dir string with the path to the directory with intermediate data of experiments (qs files).
+#' If set to NULL (default) intermediate data is not saved/read in.
+#' @param partial_run logical flag indicating if the pipeline should be run partially (from the step defined with `start_from`)
+#' @param start_from string indicating the pipeline step from which partial run should be launched
+#' @param selected_experiments character vector with experiments for which pipeline should be run.
+#' This option works only for the pipeline being run partially (i.e. with `partial_run` flag set to `TRUE`)
 #'
 #' @details
 #' \code{runDrugResponseProcessingPipeline} is made up of 3 separate steps:
@@ -64,6 +70,13 @@
 #'
 #' For fit_SE, take the averaged assay and fit curves to obtain metrics, one set of metrics for each
 #' normalization type set.
+#' 
+#' Pipeline can be run partially with `partial_run` flag set to TRUE. The `start_from` string defines the step 
+#' from which the pipeline will be launched. However, partial run of the pipeline is possible only if the whole
+#' pipeline was launched at least once with defined `data_dir` and intermediate data was saved as qs files into `data_dir`. 
+#' 
+#' Pipeline can be run for the selected experiments by changing the default value of `selected_experiments` param`.
+#' This scenario only works when `partial_run` is enabled.
 #'
 #' @name runDrugResponseProcessingPipelineFxns
 #'
@@ -78,7 +91,7 @@ create_and_normalize_SE <- function(df_,
                                       mean(x, trim = 0.25)
                                     },
                                     nested_identifiers = NULL,
-                                    nested_confounders = gDRutils::get_env_identifiers("barcode"),
+                                    nested_confounders = gDRutils::get_env_identifiers("barcode"), 
                                     override_untrt_controls = NULL,
                                     ndigit_rounding = 4,
                                     control_assay = "Controls",
@@ -131,7 +144,11 @@ runDrugResponseProcessingPipeline <- function(df_,
                                               normalized_assay = "Normalized",
                                               averaged_assay = "Averaged",
                                               metrics_assay = "Metrics",
-                                              add_raw_data = FALSE) {
+                                              add_raw_data = FALSE,
+                                              data_dir = NULL,
+                                              partial_run = FALSE,
+                                              start_from = get_pipeline_steps()[1],
+                                              selected_experiments = NULL) {
   
   checkmate::assert_data_frame(df_)
   checkmate::assert_true(any(gDRutils::get_env_identifiers("untreated_tag") %in%
@@ -149,82 +166,290 @@ runDrugResponseProcessingPipeline <- function(df_,
   checkmate::assert_string(normalized_assay)
   checkmate::assert_string(averaged_assay)
   checkmate::assert_string(metrics_assay)
+  checkmate::assert_flag(add_raw_data)
+  if (!is.null(data_dir)) {
+    checkmate::assert_directory(data_dir)
+  } 
+  checkmate::assert_flag(partial_run)
+  checkmate::assert_string(start_from)
+  checkmate::assert_choice(start_from, get_pipeline_steps())
+  checkmate::assert_character(selected_experiments, null.ok = TRUE)
+  checkmate::assert_subset(selected_experiments, names(gDRutils::get_experiment_groups()),)
   
-  nested_confounders <- if (!is.null(nested_confounders) &&
-                            any(!nested_confounders %in% names(df_))) {
-    warning(sprintf("'%s' nested confounder(s) is/are not present in the data.
-    Switching into '%s' nested confounder(s).", setdiff(nested_confounders, names(df_)),
-                    intersect(nested_confounders, names(df_))))
-    confounders_intersect <- intersect(nested_confounders, names(df_))
-    if (length(confounders_intersect) == 0) {
-      NULL
+  if (!is.null(selected_experiments) && !partial_run) {
+    stop("Selected experiments are only supported with partial_run enabled")
+  } 
+  
+  if (is.null(data_dir) && partial_run) {
+    stop("Path for/to the intermediate data is required with partial_run enabled")
+  } 
+  
+  inl <- prepare_input(df_, nested_confounders, nested_identifiers)
+ 
+  # sel - list with all experiments data
+  # se - list with single experiment data 
+  se <- list()
+  sel <- list()
+  
+  for (experiment in names(inl$df_list)) {
+    
+    # skip pipeline for not selected experiments (optionally)
+    # in detail: read intermediate data from the last step
+    if (!is.null(selected_experiments) &&
+        !(experiment %in% selected_experiments) &&
+        partial_run) {
+      
+      message("Reading intermediate data for ", experiment)
+    sel[[experiment]] <- read_intermediate_data(data_dir, tail(get_pipeline_steps(), n = 1), experiment)
+      
     } else {
-      confounders_intersect
-    }
-  } else {
-    nested_confounders
-  }
-  df_ <- identify_data_type(df_)
-  df_list <- split_raw_data(df_)
-  mae <- MultiAssayExperiment::MultiAssayExperiment()
-  if (!is.list(nested_identifiers)) {
-    nested_identifiers <- if (is.null(nested_identifiers)) {
-      list(`matrix` = .get_default_combo_identifiers(),
-           `single-agent` = .get_default_single_agent_identifiers())
-    } else if (length(nested_identifiers) == 1 || names(df_list) == 1) {
-        list(`single-agent` = nested_identifiers)
-    } else {
-      stop("Number of detected data types is greater that 1.
-           Please provide a named list of nested_identifiers")
-    }
-  }
-  for (experiment in names(df_list)) {
+    
     message("Processing ", experiment)
-    experiment_identifier <- if (experiment %in% names(nested_identifiers)) {
-      nested_identifiers[[experiment]]
+    experiment_identifier <- if (experiment %in% names(inl$nested_identifiers)) {
+      inl$nested_identifiers[[experiment]]
     } else {
-      nested_identifiers[["single-agent"]]
+      inl$nested_identifiers[["single-agent"]]
     }
-    se <- purrr::quietly(create_and_normalize_SE)(df_ = df_list[[experiment]],
+    
+    
+    if (!partial_run || !do_skip_step("create_and_normalize_SE", start_from)) {
+    se <- purrr::quietly(create_and_normalize_SE)(df_ = inl$df_list[[experiment]],
                                                      readout = readout,
                                                      control_mean_fxn = control_mean_fxn,
                                                      nested_identifiers = experiment_identifier,
-                                                     nested_confounders = nested_confounders,
+                                                     nested_confounders = inl$nested_confounders,
                                                      override_untrt_controls = override_untrt_controls,
-                                                     control_assay = control_assay, 
-                                                     raw_treated_assay = raw_treated_assay, 
+                                                     control_assay = control_assay,
+                                                     raw_treated_assay = raw_treated_assay,
                                                      normalized_assay = normalized_assay,
                                                      ndigit_rounding = ndigit_rounding)
     
-    paste_warnings(se$warnings)
-    se <- purrr::quietly(average_SE)(se = se$result, 
+    if (add_raw_data) {
+      se$result <-
+        gDRutils::set_SE_experiment_raw_data(se$result, inl$df_list[[experiment]])
+    }
+    
+      if (!is.null(data_dir)) {
+        save_intermediate_data(data_dir, "create_and_normalize_SE", experiment, se$result)
+      }
+    } else {
+       if (is_preceding_step("create_and_normalize_SE", start_from)) {
+        se$result <- read_intermediate_data(data_dir, "create_and_normalize_SE", experiment)
+       }
+    }
+    
+    if (!partial_run || !do_skip_step("average_SE", start_from)) {
+    
+      paste_warnings(se$warnings)
+      se <- purrr::quietly(average_SE)(se = se$result, 
                                         series_identifiers = experiment_identifier,
                                         override_masked = override_masked, 
                                         normalized_assay = normalized_assay, 
                                         averaged_assay = averaged_assay)
-    paste_warnings(se$warnings)
-    se <- if (experiment == "matrix") {
-      purrr::quietly(fit_SE.combinations)(se = se$result,
-                          series_identifiers = experiment_identifier,
-                          averaged_assay = averaged_assay)
+      if (!is.null(data_dir)) {
+        save_intermediate_data(data_dir, "average_SE", experiment, se$result)
+      }
+      paste_warnings(se$warnings)
     } else {
-      purrr::quietly(fit_SE)(se = se$result, 
-             nested_identifiers = experiment_identifier,
-             averaged_assay = averaged_assay, 
-             metrics_assay = metrics_assay, 
-             n_point_cutoff = n_point_cutoff)
+       if (is_preceding_step("average_SE", start_from)) {
+        se$result <- read_intermediate_data(data_dir, "average_SE", experiment)
+       }
     }
+    
+    if (!partial_run || !do_skip_step("fit_SE", start_from)) {
+      se <- if (experiment == "matrix") {
+        purrr::quietly(fit_SE.combinations)(se = se$result,
+                            series_identifiers = experiment_identifier,
+                            averaged_assay = averaged_assay)
+        
+      } else {
+        purrr::quietly(fit_SE)(se = se$result, 
+               nested_identifiers = experiment_identifier,
+               averaged_assay = averaged_assay, 
+               metrics_assay = metrics_assay, 
+               n_point_cutoff = n_point_cutoff)
+      }
+      if (!is.null(data_dir)) {
+        save_intermediate_data(data_dir, "fit_SE", experiment, se$result)
+      }
+    }
+    
     paste_warnings(se$warnings)
-    if (add_raw_data) {
-      se$result <- gDRutils::set_SE_experiment_raw_data(se$result, df_list[[experiment]])
+    sel[[experiment]] <- se$result
+    se <- list()
     }
-    mae <- c(mae, MultiAssayExperiment::MultiAssayExperiment(experiments = list(experiment = se$result)))
-    names(mae)[length(names(mae))] <- experiment
   }
-  mae
+  
+  MultiAssayExperiment::MultiAssayExperiment(experiments = sel)
+}
+
+# Prepare input data common for all experiments
+#
+# Current steps
+# - refining nested confounders
+# - refining nested identifiers
+# - splitting df_ into (per experiment) df_list
+
+#' @rdname runDrugResponseProcessingPipelineFxns
+#' @keywords internal
+prepare_input <-
+  function(df_,
+           nested_confounders,
+           nested_identifiers) {
+
+    checkmate::assert_data_frame(df_, min.rows = 1, min.cols = 1)    
+    checkmate::assert_character(nested_confounders, null.ok = TRUE)
+    checkmate::assert_character(nested_identifiers, null.ok = TRUE)
+    
+    inl <- list(
+      df_ = NULL,
+      df_list = NULL,
+      nested_confounders = NULL,
+      nested_identifiers = NULL
+    )
+    
+    inl$nested_confounders <- if (!is.null(nested_confounders) &&
+                              any(!nested_confounders %in% names(df_))) {
+      warning(
+        sprintf(
+          "'%s' nested confounder(s) is/are not present in the data.
+    Switching into '%s' nested confounder(s).",
+          setdiff(nested_confounders, names(df_)),
+          intersect(nested_confounders, names(df_))
+        )
+      )
+      confounders_intersect <-
+        intersect(nested_confounders, names(df_))
+      if (length(confounders_intersect) == 0) {
+        NULL
+      } else {
+        confounders_intersect
+      }
+    } else {
+      nested_confounders
+    }
+    inl$df_ <- identify_data_type(df_)
+    inl$df_list <- split_raw_data(inl$df_)
+    if (!is.list(nested_identifiers)) {
+      inl$nested_identifiers <- if (is.null(nested_identifiers)) {
+        list(`matrix` = .get_default_combo_identifiers(),
+             `single-agent` = .get_default_single_agent_identifiers())
+      } else if (length(nested_identifiers) == 1 ||
+                 names(inl$df_list) == 1) {
+        list(`single-agent` = nested_identifiers)
+      } else {
+        stop(
+          "Number of detected data types is greater that 1.
+           Please provide a named list of nested_identifiers"
+        )
+      }
+    }
+    
+    inl$exps <- lapply(names(inl$df_list), function(x) {
+      NULL
+    })
+    names(inl$exps) <- names(inl$df_list)
+    
+    inl
+  }
+
+
+#' get pipeline steps
+#' 
+#' @keywords internal
+get_pipeline_steps <-
+  function() {
+    c("create_and_normalize_SE",
+      "average_SE",
+      "fit_SE")
+  }
+
+#' check if the given step can be skipped if partial run is chosen
+#' 
+#' @param current_step, string with the step to be evaluated
+#' @param start_from string indicating the pipeline step from which partial run should be launched
+#' @param steps charvect with all available steps
+#' 
+#' @keywords internal
+#' @return logical
+#' 
+do_skip_step <-
+  function(current_step, start_from, steps = get_pipeline_steps()) {
+    
+    checkmate::assert_choice(current_step, steps)
+    checkmate::assert_choice(start_from, steps, null.ok = TRUE)
+    checkmate::assert_character(steps)
+    
+    if (is.null(start_from)) {
+      FALSE
+    } else {
+      c_idx <- which(steps %in% current_step)
+      s_idx <- which(steps %in% start_from)
+      c_idx < s_idx
+    }
+  }
+
+#' check if the given step is preceding the step chosen in the partial run
+#' 
+#' @param current_step, string with the step to be evaluated
+#' @param start_from string indicating the pipeline step from which partial run should be launched
+#' @param steps charvect with all available steps
+#' 
+#' @keywords internal
+#' @return logical
+#' 
+is_preceding_step <-
+  function(current_step, start_from, steps = get_pipeline_steps()) {
+   
+    checkmate::assert_choice(current_step, steps)
+    checkmate::assert_choice(start_from, steps)
+    checkmate::assert_character(steps)
+    
+    c_idx <- which(steps %in% current_step)
+    s_idx <- which(steps %in% start_from)
+    s_idx - c_idx == 1
+  }
+
+#' save intermediate data for the given experiment and step to qs file
+#' 
+#' @param path string with the save directory for the qs file 
+#' @param step, string with the step name
+#' @param experiment string with the experiment name
+#' @param se output se 
+#' 
+#' @keywords internal
+#' 
+save_intermediate_data <- function(path, step, experiment, se) {
+  
+  checkmate::assert_directory(path, "rw")
+  checkmate::assert_string(step)
+  checkmate::assert_string(experiment)
+  
+  fpath <- file.path(path, paste0(experiment, "__", step, ".qs"))
+  qs::qsave(se, fpath)
+}
+
+#' read intermediate data for the given experiment and step to qs file
+#' 
+#' @param path string with the input directory of the qs file 
+#' @param step, string with the step name
+#' @param experiment string with the experiment name
+#' 
+#' @keywords internal
+#' @return se
+#' 
+read_intermediate_data <- function(path, step, experiment) {
+  
+  checkmate::assert_directory(path, "r")
+  checkmate::assert_string(step)
+  checkmate::assert_string(experiment)
+  
+  fpath <- file.path(path, paste0(experiment, "__", step, ".qs"))
+  qs::qread(fpath)
 }
 
 #' @keywords internal
 paste_warnings <- function(list, sep = "\n") {
   warning(paste0(list, sep = sep), call. = FALSE)
 }
+
