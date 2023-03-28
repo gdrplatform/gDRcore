f03b25a131522c3b6c7282c62ddb1b0bbc24ca1e
czechb3 <bartosz.czech@contractors.roche.com>
2023-03-28 12:19:23 +0200

    test: update unit tests

--- gDRcore/tests/testthat/test-finalMAE_combo_matrix.R
+++ gDRcore/tests/testthat/test-finalMAE_combo_matrix.R
@@ -1,7 +1,9 @@
 test_that("combo_matrix: test_synthetic_data", {
   data <- "finalMAE_combo_matrix.RDS"
   original <- get_synthetic_data(data)
-  mae <- purrr::quietly(gDRtestData::generateComboMatrix)(cell_lines, drugs, e_inf, ec50, hill_coef, FALSE)
+  
+  set.seed(2)
+  mae <- purrr::quietly(gDRtestData::generateComboMatrix)(cell_lines, drugs, FALSE)
   expect_length(mae$warnings, 4)
   test_synthetic_data(original, mae$result, data)
 })
--- gDRcore/tests/testthat/test-finalMAE_combo_matrix_small.R
+++ gDRcore/tests/testthat/test-finalMAE_combo_matrix_small.R
@@ -1,8 +1,10 @@
 test_that("combo_matrix_small: test_synthetic_data", {
   data <- "finalMAE_combo_matrix_small.RDS"
   original <- get_synthetic_data(data)
+  
+  set.seed(2)
   mae <- purrr::quietly(gDRtestData::generateComboMatrixSmall)(
-    cell_lines, drugs, e_inf, ec50, hill_coef, FALSE
+    cell_lines, drugs, FALSE
   )
   expect_length(mae$warnings, 4)
 
