fb0176aabddfdd94ff9f9cdadb37b2e1e3472ab5
czechb3 <bartosz.czech@contractors.roche.com>
2023-03-28 16:19:36 +0200

    ci: rebase

--- gDRcore/tests/testthat/test-finalMAE_combo_2dose_nonoise.R
+++ gDRcore/tests/testthat/test-finalMAE_combo_2dose_nonoise.R
@@ -1,12 +1,12 @@
-test_that("combo_matrix_small: test_synthetic_data", {
-  data <- "finalMAE_combo_matrix_small.RDS"
+test_that("combo_2dose_nonoise: test_synthetic_data", {
+  data <- "finalMAE_combo_2dose_nonoise.RDS"
   original <- get_synthetic_data(data)
   
   set.seed(2)
-  mae <- purrr::quietly(gDRtestData::generateComboMatrixSmall)(
+  mae <- purrr::quietly(gDRtestData::generateComboNoNoiseData)(
     cell_lines, drugs, FALSE
   )
   expect_length(mae$warnings, 4)
-
+  
   test_synthetic_data(original, mae$result, data)
 })
