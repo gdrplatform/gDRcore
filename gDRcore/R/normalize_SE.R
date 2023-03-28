4fe85a40f47be51da94d0e0ef046927c87753fc8
czechb3 <bartosz.czech@contractors.roche.com>
2023-03-28 09:49:37 +0200

    fix: averaging including empty nested_confounders

--- gDRcore/R/normalize_SE.R
+++ gDRcore/R/normalize_SE.R
@@ -141,9 +141,15 @@ aggregate_ref <- function(ref_df, control_mean_fxn) {
   ref_cols <- data.table::as.data.table(ref_df[, data_columns, drop = FALSE])
   group_cols <- ref_cols[, setdiff(names(ref_cols), "CorrectedReadout")]
   
+  aggregate_formula <- if (length(group_cols) == 1) {
+    as.formula(". ~ control_type")
+  } else {
+    as.formula(paste0(setdiff(group_cols, "control_type"), " ~ control_type"))
+  }
+  
   ref_df_aggregate <- ref_cols[, .(x = control_mean_fxn(CorrectedReadout)), by = eval(group_cols)]
   ref_df_dcast <- data.table::dcast(ref_df_aggregate,
-                                    as.formula(paste0(setdiff(group_cols, "control_type"), " ~ control_type")),
+                                    aggregate_formula,
                                     value.var = "x")
   ref_df_dcast[!rowSums(is.na(ref_df_dcast)) >= length(setdiff(names(ref_df_dcast), group_cols)), ]
 }
