84b04e1707df73b9f8518fd1b6cb7ca9b7c7e4c4
Dominik Zabinski <dominik.zabinski@contractors.roche.com>
2023-05-02 03:55:05 -0700

    feat: usage of apply_bumpy_function in fit_SE

--- R/fit_SE.R
+++ R/fit_SE.R
@@ -24,10 +24,7 @@ fit_SE <- function(se,
   checkmate::assert_number(pcutoff)
   checkmate::assert_number(cap)
   checkmate::assert_character(curve_type)
-  
-  req_assays <- c(averaged_assay)
-  lapply(req_assays, function(x) gDRutils::validate_se_assay_name(se, x))
-
+  gDRutils::validate_se_assay_name(se, averaged_assay)
   
   if (is.null(nested_identifiers)) {
     nested_identifiers <- get_default_nested_identifiers(
@@ -36,36 +33,26 @@ fit_SE <- function(se,
     )
   }
   
-  
   metric_cols <- c(
     gDRutils::get_header("response_metrics"), 
     "maxlog10Concentration", 
     "N_conc"
   )
-  avg_trt <- BumpyMatrix::unsplitAsDataFrame(
-    SummarizedExperiment::assay(se, averaged_assay)
-  )
-  iterator <- unique(avg_trt[, c("column", "row")])
   
-  out <- gDRutils::loop(seq_len(nrow(iterator)), function(row) {
-    x <- iterator[row, ]
-    i <- x[["row"]]
-    j <- x[["column"]]
- 
-    avg_df <- avg_trt[avg_trt$row == i & avg_trt$column == j, ]
-
+  # declare fitting function
+  .fit_fun <- function(x, conc = gDRutils::get_env_identifiers("concentration"))
+  {
     fit_df <- S4Vectors::DataFrame(matrix(NA, 2, length(metric_cols)))
     colnames(fit_df) <- metric_cols
     rownames(fit_df) <- c("RV", "GR")
-
-    if (!is.null(avg_df) && all(dim(avg_df) > 0)) {
-      conc <- gDRutils::get_env_identifiers("concentration")
-      if (!all(is.na(avg_df[[conc]]))) {
-        avg_df <- avg_df[avg_df[[conc]] != 0, ]
+    
+    if (!is.null(x) && all(dim(x) > 0)) {
+      if (!all(is.na(x[[conc]]))) {
+        x <- x[x[[conc]] != 0, ]
       }
       
       fit_df <- S4Vectors::DataFrame(gDRutils::fit_curves(
-        ((avg_df)),
+        ((x)),
         series_identifiers = nested_identifiers,
         e_0 = 1,
         GR_0 = 1,
@@ -76,22 +63,14 @@ fit_SE <- function(se,
         cap = cap,
         normalization_type = curve_type))
     }
-
-    if (nrow(fit_df) != 0L) {
-      fit_df$row_id <- i
-      fit_df$col_id <- j
-    }
     fit_df
-  })
-
-  out <- S4Vectors::DataFrame(do.call("rbind", out))
-  metrics <- BumpyMatrix::splitAsBumpyMatrix(
-    out[!colnames(out) %in% c("row_id", "col_id")], 
-    row = out$row_id, 
-    col = out$col_id
-  )
-
-  SummarizedExperiment::assay(se, metrics_assay) <- metrics
+  }
+  
+  se <- apply_bumpy_function(se = se,
+                             FUN = .fit_fun,
+                             req_assay_name = averaged_assay, 
+                             out_assay_name = metrics_assay)
+  
   se <- gDRutils::set_SE_fit_parameters(se, 
     value = list(
       n_point_cutoff = n_point_cutoff,
