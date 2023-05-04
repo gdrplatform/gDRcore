0a93b02cf2e47d58ceec10d82dc4a3d935502445
Dominik Zabinski <dominik.zabinski@contractors.roche.com>
2023-05-04 05:33:52 -0700

    test: creating more flexible tests

--- tests/testthat/test-fit_SE.R
+++ tests/testthat/test-fit_SE.R
@@ -26,10 +26,11 @@ test_that("fit_SE errors as expected", {
                                  "finalMAE_combo_2dose_nonoise2.RDS", package = "gDRtestData")
   )
   
-  nrm_se <- normalize_SE(se = maeReal[[2]], data_type = "single-agent")
-  avg_se <- average_SE(se = nrm_se, data_type = "single-agent")
+  ll <- names(maeReal@ExperimentList)
+  se <- maeReal[[ll[which(ll == "single-agent")[1]]]]
+  assay(se, "Metrics") <- NULL
   
-  expect_error(fit_SE(avg_se, averaged_assay = "dummy"), 
+  expect_error(fit_SE(se, averaged_assay = "dummy"), 
                "'dummy' is not on of the available assays")
 })
 
@@ -38,10 +39,14 @@ test_that("fit_SE works as expected", {
                                  "finalMAE_combo_2dose_nonoise2.RDS", package = "gDRtestData")
   )
   
-  nrm_se <- normalize_SE(se = maeReal[[2]], data_type = "single-agent")
-  avg_se <- average_SE(se = nrm_se, data_type = "single-agent")
-  fit_se <- fit_SE(avg_se)
+  ll <- names(maeReal@ExperimentList)
+  se <- maeReal[[ll[which(ll == "single-agent")[1]]]]
+  assay(se, "Metrics") <- NULL
+  ext_ass <- assayNames(se)
+  
+  fit_se <- fit_SE(se, metrics_assay = "testing")
   expect_class(fit_se, "SummarizedExperiment")
+  expect_identical(assayNames(fit_se), c(ext_ass, "testing"))
 })
 
 test_that("fit_SE.combinations works as expected", {
