87884fc763f609c5459e2d4062ddef4a2801d2f9
czechb3 <bartosz.w.czech@gmail.com>
2024-04-02 13:31:57 +0200

    doc: update namespace (II)

--- tests/testthat/test-convert_se_to_raw_data.R
+++ tests/testthat/test-convert_se_to_raw_data.R
@@ -33,7 +33,7 @@ test_that("convert_mae_to_raw_data works as expected with matrix data", {
   test_synthetic_data(original, mae$result, data)
   test_synthetic_data(mae$result, mae2$result, data)
   
-  trt <- BumpyMatrix::unsplitAsDataFrame(assay(original[[1]], "RawTreated"))
+  trt <- BumpyMatrix::unsplitAsDataFrame(SummarizedExperiment::assay(original[[1]], "RawTreated"))
   trt <- trt[!duplicated(trt$record_id), ]
   df_raw1 <- convert_mae_to_raw_data(original)
   SummarizedExperiment::assay(original[[1]], "RawTreated") <- BumpyMatrix::splitAsBumpyMatrix(
