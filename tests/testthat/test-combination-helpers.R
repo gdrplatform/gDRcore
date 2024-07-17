e7b5f9ef2c6e204e60f202b1a3e3e17bafe17746
czechb3 <bartosz.w.czech@gmail.com>
2024-07-17 12:18:36 +0200

    refactor: update unit tests

--- tests/testthat/test-combination-helpers.R
+++ tests/testthat/test-combination-helpers.R
@@ -34,6 +34,21 @@ test_that("calculate_excess works as expected", {
 })
 
 
+test_that("calculate_score works as expected", {
+  metric <- data.table::data.table(Concentration = c(1, 2, 3, 1, 2, 3),
+                                   Concentration_2 = c(1, 1, 1, 2, 2, 2),
+                                   GRvalue = c(100, 200, 300, 400, 500, 600))
+  measured <- data.table::data.table(Concentration = c(3, 1, 2, 2, 1, 3),
+                                     Concentration_2 = c(1, 1, 1, 2, 2, 2),
+                                     testvalue = c(200, 0, 100, 400, 300, 500))
+  series_identifiers <- c("Concentration", "Concentration_2")
+  metric_col <- "GRvalue"
+  measured_col <- "testvalue"
+  obs <- calculate_excess(metric, measured, series_identifiers, metric_col, measured_col)
+  score <- calculate_score(obs$x)
+  expect_equal(score, 100)
+})
+
 
 test_that(".calculate_dilution_ratio works as expected", {
   ratio <- 0.5
