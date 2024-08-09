94e6f1dca3b1a0f55a6f54dab49777a343333405
bczech <bartosz.czech@contractors.roche.com>
2024-08-09 03:59:55 -0700

    refactor: add unit tests

--- tests/testthat/test-map_df.R
+++ tests/testthat/test-map_df.R
@@ -51,7 +51,7 @@ test_that("map_df works as expected", {
     Gnumber = c("Drug12345678", "Drug12345678", "Drug12345678", "Drug87654321", "Drug87654321", "Drug87654321"),
     DrugName = c("DrugX", "DrugX", "DrugX", "DrugY", "DrugY", "DrugY"),
     drug_moa = c("MOA1", "MOA1", "MOA1", "MOA2", "MOA2", "MOA2"),
-    Duration = c(0, 168, 168, 0, 168, 168),
+    Duration = c(0, 120, 120, 0, 120, 120),
     E2 = c("0", "0", "0.0023", "0", "0", "0.0023"),
     rn = c("3", "4", "5", "8", "9", "10")
   )
@@ -67,6 +67,18 @@ test_that("map_df works as expected", {
                                         override_untrt_controls = c(E2 = 0.0023))
   expect_list(map_override_untrt_controls)
   expect_length(map_override_untrt_controls, 4)
+  expect_equal(as.numeric(unlist(map_override_untrt_controls)), c(3, 3, 8, 8))
+  
+  
+  map_override_untrt_controls2 <- map_df(trt_dt,
+                                         ref_dt,
+                                         ref_cols = ref_cols,
+                                         ref_type = ref_type,
+                                         override_untrt_controls = NULL)
+  
+  expect_list(map_override_untrt_controls2)
+  expect_length(map_override_untrt_controls2, 4)
+  expect_equal(as.numeric(unlist(map_override_untrt_controls2)), c(3, 3, 8, 8))
 })
 
 
