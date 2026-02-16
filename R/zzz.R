2fd7d89ec57d2151f409c41939bc702bd076f8dc
Dariusz Scigocki <daro.scig@gmail.com>
2026-02-16 12:28:57 +0000

    fix: move global logic to .onLoad and resolve stack imbalance

---
+++ R/zzz.R
@@ -0,0 +1,71 @@
+
+.drugNameRegex <- NULL
+.untreated_tag_patterns <- NULL
+.untreatedDrugNameRegex <- NULL
+patterns <- NULL
+
+#' onload function
+#'
+#' @param libname library name
+#' @param pkgname package name
+#' @noRd
+.onLoad <- function(libname, pkgname) {
+  # scientific notation was disabled due to the problem with unit tests
+  options(scipen = 999) 
+  
+  cores <- Sys.getenv("NUM_CORES")
+  # based on https://github.com/Bioconductor/BiocParallel/issues/98
+  if (.Platform$OS.type != "windows" && cores != "") {
+    BiocParallel::register(
+      BiocParallel::MulticoreParam(workers = as.numeric(cores)), 
+      default = TRUE
+    )
+  } else { 
+    BiocParallel::register(
+      BiocParallel::SerialParam(), 
+      default = TRUE
+    )
+  } 
+  # CONS
+  .drugNameRegex <<- sprintf("^%s$|^%s_[[:digit:]]+$",
+                            gDRutils::get_env_identifiers("drug_name"),
+                            gDRutils::get_env_identifiers("drug_name"))
+  
+  .untreated_tag_patterns <<- vapply(
+    gDRutils::get_env_identifiers("untreated_tag"),
+    sprintf,
+    fmt = "^%s$",
+    character(1)
+  )
+  .untreatedDrugNameRegex <<- paste(.untreated_tag_patterns, collapse = "|")
+  
+  # data.table compatible
+  patterns <<- data.table:::patterns
+  utils::globalVariables(
+    c(
+      ".",
+      "..cotrt_var",
+      "..present_ref_cols",
+      "..y",
+      "bliss_score",
+      "column",
+      "CorrectedReadout",
+      "cotrt_value",
+      "Duration",
+      "hsa_score",
+      "isDay0",
+      "LogFoldChange",
+      "normalization_type",
+      "priority1",
+      "priority2",
+      "ratio",
+      "ReadoutValue",
+      "ReadoutValue_T0",
+      "record_id",
+      "rn",
+      "smooth",
+      "x",
+      "x.N"
+    ), 
+    pkgname)
+}
